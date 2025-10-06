97f3d864908917c39e77b885425a4af89674415d
anand-lab25 <anand.business1988@gmail.com>
2025-10-06 15:32:07 +0000

    infra: exposed validator reward/penalty/slashing metrics via Prometheus for monitoring and fault analysis

--- sim/validator.go
+++ sim/validator.go
@@ -3,7 +3,32 @@ import (
 	"fmt"
 	"time"
 	"math/rand"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
+
+var (
+	rewardGuage = prometheuse.NewGauge(prometheuse.GaugeOpts{
+		Name:"validator_reward_total",
+		Help: "total reward earned by Validators",
+	})
+	penaltyGauge = prometheuse.NewGauge(prometheuse.GaugeOpts{
+		Name:"validator_penalty_total",
+		Help:"total penalty levied on a block",
+	})
+	slashedGauge = prometheuse.NewGauge(prometheuse.GaugeOpts{
+		Name:"validator_slashed",
+		Help:"Slashing status 1 if slashed otherwise 0",
+	})
+)
+
+
+func init() {
+    prometheuse.MustRegister(rewardGauge,penaltyGauge,slashedGauge)
+}
+
+
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
@@ -12,22 +37,22 @@ func main() {
 	var rewards, penalities int 
 	for i:=1; i <= 10;i++ {
       if slashed {
-		fmt.Println("block slashed halting at %d",i)
+		fmt.Println("block slashed halting at %d\n",i)
 	  }
 	switch {
 
 	case rand.Float64() < 0.2: 
-			fmt.Println("⚠️ Block %d missed sining \n",i)
+			fmt.Println("⚠️ Block %d missed signing\n",i)
 			penalities += 10
 	case rand.Float64() < 0.1: 
-            fmt.Println("❌ Block %d double signed - slashing triggered \n",i)
+            fmt.Println("❌ Block %d double signed - slashing triggered\n",i)
 			slashed = true
 			penalities += 100
 	default:		
-		fmt.Printf("Validator successfuly signed blocked %d \n",i)
+		fmt.Printf("Validator successfuly signed block %d\n",i)
 		rewards += 20
 	}
-    time.Sleep(1*time.second)
+    time.Sleep(1*time.Second)
     }
 	fmt.Println("\n 📊 final validator summary")
 	fmt.printf("🔒 Slashed: %v\n",slashed)
